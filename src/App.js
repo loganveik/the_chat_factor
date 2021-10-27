1fcccd52f42d6f2d2fb4857d1b5483096317ee20
ljv29 <jumpswervelogan@gmail.com>
2021-10-27 00:59:52 -0700

    created routes in app.js for all the 4 pages that are yet to made

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import loginPage from './pages/login/login';
+import signupPage from './pages/signup/signup';
+import createPage from './pages/create/create';
+import libraryPage from './pages/library/library';
 
 function App() {
   return (
     <div className="App">
-      hello
+      <Switch>
+        <Route exact path='/' component={}/>
+        <Route path='/signup' component={}/>
+        <Route path='create' component={}/>
+        <Route path='library' component={}/>
+      </Switch>
     </div>
   );
 }
